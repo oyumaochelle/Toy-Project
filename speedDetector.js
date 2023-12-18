c141c7b7a38f9384c5d38a2ecc470cd46f423735
ochelle <alexizochelle@gmail.com>
2023-12-18 22:47:29 +0300

    Completed second challenge

---
+++ speedDetector.js
@@ -0,0 +1,20 @@
+// Step 1: Get the speed input
+function speedDetector(speed){
+    // Step 2: Set the speed limit
+    const speedLimit = 70;
+
+    // Step 3: Check if the speed is less than the speed limit
+    if (speed < speedLimit) {
+        return("Ok");
+    } else {
+        const demeritPoints = Math.floor((speed - speedLimit) / 5); // Step 4: Calculate demerit points for exceeding the speed limit
+        if (demeritPoints > 12) {       // Step 5: Check if demerit points exceed 12 (license suspension threshold)
+            return ("License suspended");
+            } else {
+                return ("Points: " + demeritPoints); // Step 6: Print the total demerit points
+            }
+    }
+    }
+console.log(speedDetector(90))
+
+
