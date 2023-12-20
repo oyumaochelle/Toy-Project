cadf117a9a0ba607dae30389efea84a7a4e9c878
ochelle <alexizochelle@gmail.com>
2023-12-20 14:32:59 +0300

    Updatimg the file

---
+++ netSalary.js
@@ -0,0 +1,64 @@
+// Declare constants for different deduction rates
+const KRA_TAX_RATES = {
+    "Under Ksh. 24,000": 10,
+    "Ksh. 24,001 - Ksh. 32,333": 25,
+    "Ksh. 32,334 - Ksh. 500,000": 30,
+    "Ksh. 500,001 - Ksh. 800,000": 32.5,
+    "Above Ksh. 800,000": 35,
+};
+const NHIF_TAX = {
+    "Under Ksh. 5,999": 150,
+    "Ksh. 6,000 - Ksh. 7,999": 300,
+    "Ksh. 8,000 - Ksh. 11,999": 400,
+    "Ksh. 12,0000 - Ksh. 14,999": 500,
+    "Ksh. 15,000 - Ksh. 19,999": 600,
+    "Ksh. 20,000 - Ksh. 24,999": 750,
+    "Ksh. 25,000 - Ksh. 29,999": 850,
+    "Ksh. 30,000 - Ksh. 34,999": 900,
+    "Ksh. 35,000 - Ksh. 39,999": 950,
+    "Ksh. 40,000 - Ksh. 44,999": 1000,
+    "Ksh. 45,000 - Ksh. 49,999": 1100,
+    "Ksh. 50,000 - Ksh. 59,999": 1200,
+    "Ksh. 60,000 - Ksh. 69,999": 1300,
+    "Ksh. 70,000 - Ksh. 79,999": 1400,
+    "Ksh. 80,000 - Ksh. 89,999": 1500,
+    "Ksh. 90,000 - Ksh. 99,999": 1600,
+    "Above Ksh.100,000": 300,
+}
+const NSSF_RATE = 6;
+
+function calculateNetSalary(basicSalary, benefits) {
+    // Calculate gross salary (basic + benefits)
+    const grossSalary = basicSalary + benefits;
+    
+    // Determine applicable KRA tax rate based on gross salary
+    let kraTaxRate = 0;
+    for (const [range, rate] of Object.entries(KRA_TAX_RATES)) {
+    if (grossSalary <= parseInt(range.split(" - ")[1])) {
+        kraTaxRate = rate;
+        break;
+    }
+}
+    // Calculate deductions
+    const kraTax = ((grossSalary * kraTaxRate) / 100);
+    const nhifDeduction = ?
+    const nssfDeduction = ((grossSalary * NSSF_RATE) / 100);
+
+    // Calculate net salary (gross - deductions)
+    const netSalary = grossSalary - kraTax - nhifDeduction - nssfDeduction;
+
+    // Return salary details
+    return {
+        grossSalary,
+        kraTax,
+        nhifDeduction,
+        nssfDeduction,
+        netSalary,
+    };
+}
+const salaryDetails = calculateNetSalary(basicSalary, benefits);
+console.log("Gross Salary:", salaryDetails.grossSalary);
+console.log("KRA Tax:", salaryDetails.kraTax);
+console.log("NHIF Deduction:", salaryDetails.nhifDeduction);
+console.log("NSSF Deduction:", salaryDetails.nssfDeduction);
+console.log("Net Salary:", salaryDetails.netSalary);
--- speedDetector.js
+++ speedDetector.js
@@ -1,13 +1,14 @@
 // Step 1: Get the speed input
 function speedDetector(speed){
-    // Step 2: Set the speed limit
+// Step 2: Set the speed limit
     const speedLimit = 70;
 
-    // Step 3: Check if the speed is less than the speed limit
+        // Step 3: Check if the speed is less than the speed limit
     if (speed < speedLimit) {
         return("Ok");
-    } else {
-        const demeritPoints = Math.floor((speed - speedLimit) / 5); // Step 4: Calculate demerit points for exceeding the speed limit
+        // Step 4: Calculate demerit points for exceeding the speed limit
+    } else { 
+        const demeritPoints = Math.round((speed - speedLimit) / 5); 
         if (demeritPoints > 12) {       // Step 5: Check if demerit points exceed 12 (license suspension threshold)
             return ("License suspended");
             } else {
